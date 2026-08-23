b19d26bec605b55d97d5add64a224060adf9a0d2
francesco <fviscomi@gmail.com>
2026-08-24 01:05:30 +0200

    prova

--- src/views/LezioniView.vue
+++ src/views/LezioniView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex min-h-screen items-center justify-center">
-  <h1 class="text-4xl font-bold">Lezioni View</h1>
+  <h1 class="text-4xl font-bold">Lezioni View----- under construction</h1>
 </div>
 </template>
 
