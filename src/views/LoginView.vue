8e99e5a0c0093b6306522534a2e6254fa341fb29
Francesco Viscomi <francesco.viscomi@it.experis.com>
2026-09-04 17:37:56 +0200

    ricreo user pool cancellato per errore

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,8 @@
 <template>
    <div class="bg-black min-h-screen px-4 py-12 text-slate-100">
   <Authenticator>
-    <template v-slot="{ signOut }">
+    <template v-slot="{ user,signOut }">
+      <h1> ciao {{ user.username }}</h1>
       <button @click="signOut">Sign Out</button>
     </template>
   </Authenticator>
