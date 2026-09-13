36267be38fb8964135bd4aa0d465bed85bb31173
francesco <fviscomi@gmail.com>
2026-09-13 21:43:19 +0200

    prova modifica db

--- src/views/TutorialView.vue
+++ src/views/TutorialView.vue
@@ -1,12 +1,21 @@
 <template>
-    <div class="flex min-h-screen items-center justify-center">
-  <h1 class="text-4xl font-bold">Tutorial View</h1>
-</div>
+    <!-- <div class="flex min-h-screen items-center justify-center">
+  <h1 class="text-4xl font-bold">Tutorial View</h1> -->
+  <div class="pt-16">
+
+    ffff
+  <Todos />
+  </div>
+<!-- </div> -->
 </template>
 
 <script>
+import Todos from '@/components/Todos.vue'
 export default { 
-    name:'TutorialView'
+    name:'TutorialView',
+     components: {
+    Todos,
+  },
 }
 
 </script>
