73d96d7739a9aa6837784b081b23354c6f528dbc
Francesco Viscomi <francesco.viscomi@it.experis.com>
2026-09-22 18:16:22 +0200

    inizio pagina ricerca

--- src/views/RicercaView.vue
+++ src/views/RicercaView.vue
@@ -8,23 +8,55 @@
     <div v-else>
       Utente non autenticato
     </div>
-
-    <br><br>
-
-    <h1 class="text-4xl font-bold">
-      Lezioni View ----- under construction
-    </h1>
+  
+    <ul>
+      <li 
+        v-for="esperienza in esperienze" 
+        :key="esperienza.id">
+        {{ esperienza }}
+      </li>
+    </ul>
 
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { computed } from 'vue'
+// @ts-ignore
 import { useAuthStore } from '@/components/gestioneLogin'
+import type { Schema } from "../../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import { ref,onMounted } from "vue";
+
+
+
 
 const authStore = useAuthStore()
-
 const isAuthenticated = computed(() => authStore.authenticated)
-
 const user = computed(() => authStore.user)
+
+
+const client = generateClient<Schema>();
+
+// create a reactive reference to the array of eperienze
+const esperienze = ref<Array<Schema['Activities']["type"]>>([]);
+
+// fetch todos when the component is mounted
+ onMounted(() => {
+  listEsperienze();
+});
+
+
+function listEsperienze() {
+  debugger;
+  client.models.Activities.observeQuery().subscribe({
+    next: ({ items, isSynced }) => {
+      esperienze.value = items
+      console.log(JSON.stringify(esperienze.value, null, 2));
+     },
+  }); 
+}
+
+
+
 </script>
