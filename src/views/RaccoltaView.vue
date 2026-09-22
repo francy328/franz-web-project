44c53045e5c2b801c8ccb19265a699b74f8f644c
Francesco Viscomi <francesco.viscomi@it.experis.com>
2026-09-22 16:59:26 +0200

    migliorie 3

--- src/views/RaccoltaView.vue
+++ src/views/RaccoltaView.vue
@@ -113,7 +113,7 @@ import type { Schema } from "../../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { uploadData } from "aws-amplify/storage";
 import { computed } from "vue";
-import { useAuthStore } from "../components/gestioneLogin";
+import { useAuthStore } from "@/components/gestioneLogin";
 
 const authStore = useAuthStore();
 
